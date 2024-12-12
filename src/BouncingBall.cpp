e636c889c33590c596bd03322e532ed60d75d057
Daniel Lopes <ddanielssoares@gmail.com>
2024-12-12 01:43:04 -0300

    Iluminação na bola

--- src/BouncingBall.cpp
+++ src/BouncingBall.cpp
@@ -18,7 +18,20 @@ void BouncingBall::draw() const
     glPushMatrix();
     glTranslatef(posX, posY, posZ);
     glColor3f(0.0f, 0.5f, 1.0f);
-    glutSolidSphere(radius, 20, 20);
+
+    glEnable(GL_TEXTURE_2D);
+    glBindTexture(GL_TEXTURE_2D, textureID);
+
+    float ambientHead[4] = {0.2f, 0.2f, 0.2f, 1.0f};
+    float diffuseHead[4] = {0.8f, 0.8f, 0.8f, 1.0f};
+    float specularHead[4] = {1.0f, 1.0f, 1.0f, 1.0f};
+    setMaterial(ambientHead, diffuseHead, specularHead, 32.0f);
+
+    GLUquadric *quadric = gluNewQuadric();
+    gluQuadricTexture(quadric, GL_TRUE);
+    gluQuadricNormals(quadric, GLU_SMOOTH);
+
+    gluSphere(quadric, radius, 50, 50);
     glPopMatrix();
 }
 
@@ -47,4 +60,17 @@ void BouncingBall::setPosition(float x, float y, float z)
     posX = x;
     posY = y;
     posZ = z;
+}
+
+void BouncingBall::setMaterial(const float ambient[4], const float diffuse[4], const float specular[4], float shininess) const
+{
+    glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, ambient);
+    glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, diffuse);
+    glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, specular);
+    glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, shininess);
+}
+
+void BouncingBall::init()
+{
+    // loadTexture("src/bola.png", &textureID);
 }
--- src/BouncingBall.h
+++ src/BouncingBall.h
@@ -2,6 +2,7 @@
 #define BOUNCINGBALL_H
 
 #include <GL/glut.h>
+// #include "Texture.h"
 
 class Terrain;
 class Physics;
@@ -14,11 +15,16 @@ private:
     float velocityX, velocityY, velocityZ;
     Physics *physics;
 
+    GLuint textureID;
+
+    void setMaterial(const float ambient[4], const float diffuse[4], const float specular[4], float shininess) const;
+
 public:
     BouncingBall(float radius, float initialPosX, float initialPosY, float initialPosZ);
     ~BouncingBall();
 
     void draw() const;
+    void init();
 
     void update(float deltaTime, const Terrain &terrain);
 
